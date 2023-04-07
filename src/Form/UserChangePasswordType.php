b4d78c087f87d9143d16738db390a89c92384605
cefimmac154 <cefim@Armand-Mac.local>
2023-04-07 13:12:51 +0200

    add confirm email, forget password, functionnality implementation

--- src/Form/UserChangePasswordType.php
+++ src/Form/UserChangePasswordType.php
@@ -6,20 +6,14 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
-class RegistrationType extends AbstractType
+class UserChangePasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('company', CompanyType::class)
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('email', EmailType::class)
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les mots de passe ne correspondent pas.',
---
+++ src/Service/MailerService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+
+/**
+ * Service permettant de générer un email
+ */
+class MailerService
+{
+
+    private MailerInterface $mailerInterface;
+
+    public function __construct(MailerInterface $mailerInterface)
+    {
+
+        $this->mailerInterface = $mailerInterface;
+    }
+
+    public function send(string $to, string $subject, string $templateTwig, array $context): void
+    {
+        $email = (new TemplatedEmail())
+            ->from(new Address('admin@devjob.fr', 'Administrateur'))
+            ->to($to)
+            ->subject($subject)
+            ->htmlTemplate('pages/security/' . $templateTwig)
+            ->context($context);
+
+        try {
+            $this->mailerInterface->send($email);
+        } catch (TransportExceptionInterface $transportException) {
+            throw $transportException;
+        }
+    }
+}
