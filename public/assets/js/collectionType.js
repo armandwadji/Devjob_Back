4a0ffc5a8e8db29f189f6d57e3ecf27bad9a06a3
cefimmac154 <cefim@Armand-Mac.local>
2023-06-26 21:30:54 +0200

    collectype.js issue 3

--- public/assets/js/collectionType.js
+++ public/assets/js/collectionType.js
@@ -1,77 +1,77 @@
-window.onload = () => {
+// window.onload = () => {
 
-  // ******** REQUIREMENT *********
-  let requirements = document.querySelector("#requirements-item");
-  let span = document.querySelector( "#offer_requirement_requirementItems" );
-  let newRequirementItemBtn = document.querySelector(".ajout-requirement-item");
+//   // ******** REQUIREMENT *********
+//   let requirements = document.querySelector("#requirements-item");
+//   let span = document.querySelector( "#offer_requirement_requirementItems" );
+//   let newRequirementItemBtn = document.querySelector(".ajout-requirement-item");
 
-  requirements.dataset.index = requirements.querySelector("#offer_requirement_requirementItems").childElementCount;
+//   requirements.dataset.index = requirements.querySelector("#offer_requirement_requirementItems").childElementCount;
 
-  if (requirements.dataset.index > 0) {
-    for (let i = 0; i < requirements.dataset.index; i++) {
-      const boutonSupp = createDeletebtn(i, requirements);
-      requirements.querySelector("#offer_requirement_requirementItems").childNodes[i].append(boutonSupp);
-    }
-  }
+//   if (requirements.dataset.index > 0) {
+//     for (let i = 0; i < requirements.dataset.index; i++) {
+//       const boutonSupp = createDeletebtn(i, requirements);
+//       requirements.querySelector("#offer_requirement_requirementItems").childNodes[i].append(boutonSupp);
+//     }
+//   }
 
-  // Evènement pour créer un nouveau requirement item
-  newRequirementItemBtn.addEventListener( "click", () => span.append( createNewItemForm( requirements ) ) );
+//   // Evènement pour créer un nouveau requirement item
+//   newRequirementItemBtn.addEventListener( "click", () => span.append( createNewItemForm( requirements ) ) );
 
-   // ******** ROLES ********
-  let roles = document.querySelector( '#roles-item' );
-  let spanRole= document.querySelector( "#offer_role_roleItems" );
-  let newRolesItemBtn = document.querySelector( '.ajout-role-item' );
+//    // ******** ROLES ********
+//   let roles = document.querySelector( '#roles-item' );
+//   let spanRole= document.querySelector( "#offer_role_roleItems" );
+//   let newRolesItemBtn = document.querySelector( '.ajout-role-item' );
 
-  roles.dataset.index = roles.querySelector( "#offer_role_roleItems" ).childElementCount;
+//   roles.dataset.index = roles.querySelector( "#offer_role_roleItems" ).childElementCount;
   
-  if (roles.dataset.index > 0) {
-    for (let i = 0; i < roles.dataset.index; i++) {
-      const boutonSupp = createDeletebtn(i, roles);
-      roles.querySelector("#offer_role_roleItems").childNodes[i].append(boutonSupp);
-    }
-  }
+//   if (roles.dataset.index > 0) {
+//     for (let i = 0; i < roles.dataset.index; i++) {
+//       const boutonSupp = createDeletebtn(i, roles);
+//       roles.querySelector("#offer_role_roleItems").childNodes[i].append(boutonSupp);
+//     }
+//   }
 
-  // Evènement pour créer un nouveau role item
-  newRolesItemBtn.addEventListener("click", () =>  spanRole.append(createNewItemForm(roles)) );
+//   // Evènement pour créer un nouveau role item
+//   newRolesItemBtn.addEventListener("click", () =>  spanRole.append(createNewItemForm(roles)) );
     
-};
+// };
 
-// Méthode de création du boutton supprimer d'un item
-const createDeletebtn = (index, requirements) => {
-  const boutonSupp = document.createElement("button");
-  boutonSupp.className = "btn btn-danger";
-  boutonSupp.id = `delete-ingredient-${index}`;
-  boutonSupp.setAttribute("type", "button");
+// // Méthode de création du boutton supprimer d'un item
+// const createDeletebtn = (index, requirements) => {
+//   const boutonSupp = document.createElement("button");
+//   boutonSupp.className = "btn btn-danger";
+//   boutonSupp.id = `delete-ingredient-${index}`;
+//   boutonSupp.setAttribute("type", "button");
 
-  boutonSupp.textContent = "supprimer";
+//   boutonSupp.textContent = "supprimer";
 
-  boutonSupp.addEventListener("click", (e) => {
-    if (e.target.classList.contains("btn")) {
-      e.target.parentElement.remove();
-      requirements.dataset.index--;
-    }
-  });
+//   boutonSupp.addEventListener("click", (e) => {
+//     if (e.target.classList.contains("btn")) {
+//       e.target.parentElement.remove();
+//       requirements.dataset.index--;
+//     }
+//   });
 
-  return boutonSupp;
-};
+//   return boutonSupp;
+// };
 
-// Méthode de création d'un nouvel item
-const createNewItemForm = (requirements) => {
-  let index = requirements.dataset.index;
-  let prototype = requirements.dataset.prototype.replace(/__name__/g, index);
+// // Méthode de création d'un nouvel item
+// const createNewItemForm = (requirements) => {
+//   let index = requirements.dataset.index;
+//   let prototype = requirements.dataset.prototype.replace(/__name__/g, index);
 
-  // transforme le template en html
-  let content = document.createElement("div");
-  content.innerHTML = prototype;
+//   // transforme le template en html
+//   let content = document.createElement("div");
+//   content.innerHTML = prototype;
 
-  let newForm = content.querySelector("fieldset");
+//   let newForm = content.querySelector("fieldset");
 
-  const boutonSupp = createDeletebtn(index, requirements);
+//   const boutonSupp = createDeletebtn(index, requirements);
 
-  newForm.append(boutonSupp);
+//   newForm.append(boutonSupp);
 
-  requirements.dataset.index++;
+//   requirements.dataset.index++;
 
-  return newForm;
-};
+//   return newForm;
+// };
 
